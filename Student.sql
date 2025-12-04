d935f0b46d35e47792e2fd8e423d68193c4e0055
thisminji <thisminji@dgu.ac.kr>
2025-12-04 15:21:34 +0900

    SQL : Dept table captin column add

--- Student.sql
+++ Student.sql
@@ -1,35 +1,59 @@
+create database schooldb;
+
+create user school@'%' identified by 'Schooldb1!';
+
+grant all privileges on schooldb.* to school@'%';
+
 use schooldb;
--- 학생
-CREATE TABLE Student (
-  id          INT UNSIGNED NOT NULL AUTO_INCREMENT, 
-  createdate  TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-  updatedate  TIMESTAMP    DEFAULT CURRENT_TIMESTAMP
-                          ON UPDATE CURRENT_TIMESTAMP,
-  name        VARCHAR(15)  NOT NULL,
-  birthdt     DATE         NOT NULL,
-  major       TINYINT UNSIGNED NOT NULL,
-  email       VARCHAR(255) NOT NULL,
-  mobile      VARCHAR(11)  NOT NULL, 
-  gender      BIT          NOT NULL DEFAULT b'0' COMMENT '성별(0: 남, 1: 여)',
-  graduatedat VARCHAR(10)  NULL,
+
+create table Student (
+  id int unsigned auto_increment not null,
+  createdate timestamp default current_timestamp,
+  updatedate timestamp default current_timestamp on update current_timestamp,
+  name varchar(15) not null,
+  birthdt date not null,
+  major tinyint unsigned not null,
+  email varchar(255) not null,
+  mobile varchar(11) not null,
+  gender bit not null default 0 comment '성별(0: 남, 1:여)',
+  graduatedat varchar(10) null,
   
-  PRIMARY KEY (id),
-  UNIQUE KEY unique_Student_email (email)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-show create table Student;
-
-insert into Student(name,birthdt,major,email,mobile)
-			values('Hong','2001-01-03',5);
-
-
-create table Major(
-	id tinyint unsigned auto_increment primary key,
-    name varchar(15) not null,
-    unique key uniq_Major_name (name)
+  primary key (id),
+  unique key unique_Student_email (email)
 );
 
-insert  into Major (name) values ('철학과'),('컴퓨터공학과'),('건축과');
+show create table Student;
+CREATE TABLE `Student` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `createdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedate` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthdt` date NOT NULL,
+  `major` tinyint unsigned NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mobile` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` bit(1) NOT NULL DEFAULT b'0' COMMENT '성별(0: 남, 1:여)',
+  `graduatedat` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_Student_email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+create table Major(
+  id tinyint unsigned auto_increment primary key,
+  name varchar(15) not null,
+  unique key uniq_Major_name (name)
+);
+
+insert into Major (name) values('철학과'), ('컴퓨터공학과'), ('건축과');
+
+insert into Student(name, birthdt, major, email, mobile)
+             values('Hong', '2001-01-03', 5, 'hong@gmail.com', '01012341234');
+
+alter table Student add constraint foreign key fk_Student_Major (major)
+    references Major(id);
+    
+select * from Student where major not in (select id from Major);
+select * from Major;
+update Student set major = 1 where id = 1; -- Hong
+
+show index from Student;
