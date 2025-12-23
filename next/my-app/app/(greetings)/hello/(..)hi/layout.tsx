7dc052f1dfe3a871b125fa94452760af2b27829f
thisminji <thisminji@dgu.ac.kr>
2025-12-23 12:57:59 +0900

    Next : layout 수정

--- next/my-app/app/(greetings)/hello/(..)hi/layout.tsx
+++ next/my-app/app/(greetings)/hello/(..)hi/layout.tsx
@@ -6,7 +6,8 @@ export default function HelloHiLayout({ children }: PropsWithChildren) {
   return (
     <div className="border-2 border-green-300 text-center">
       <h1 className="">Hello/(..)hi Layout</h1>
-      <div className="flex gap-3 justify-center">
+      <div className="flex justify-center gap-3">
+        {" "}
         {TIMES.map((time) => (
           <Link key={time} href={`/hi/${time}`}>
             {time}
