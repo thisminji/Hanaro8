bfd43ab91f9579e191b4eac63b9b5750fab871ab
thisminji <thisminji@dgu.ac.kr>
2025-12-23 12:55:01 +0900

    Next: @-parallel / (..) intercept

---
+++ next/my-app/app/(greetings)/hello/(..)hi/page.tsx
@@ -0,0 +1,3 @@
+export default function Hi() {
+  return <h1>Hello(..)Hi~</h1>;
+}
--- next/my-app/app/(greetings)/hi/[time]/comments/[cmt]/page.tsx
+++ next/my-app/app/(greetings)/hi/[time]/comments/[cmt]/page.tsx
@@ -1,40 +1,21 @@
-// app/hi/[time]/comments/[cmt]/page.tsx
+import { use } from "react";
+import { TIMES } from "../../../constants";
 
-//주소에서 받아올 값의 이름표 <time/cmt>
-type Params = { time: string; cmt: string };
+export const generateStaticParams = () =>
+  TIMES.flatMap((time, i) =>
+    [1, 2, 3].map((cmt) => ({ time, cmt: String(i * 3 + cmt) }))
+  );
 
-//미리 정해둔 주소 말고는 404
-export const dynamicParams = false; // generateStaticParams에 없는 건 404
-
-//미리 만들 주소 목록
-//빌드할 때 이 주소들의 HTML 미리 만들어짐
-//SSG 핵심
-export function generateStaticParams(): Params[] {
-  // rules (각 time별로 cmt 번호)
-  const rules: Record<string, number[]> = {
-    morning: [1, 2, 3],
-    afternoon: [4, 5, 6],
-    evening: [7, 8, 9],
-  };
-
-  //result 만들기
-  //주소를 하나씩 리스트로 만들기
-  const result: Params[] = [];
-  for (const [time, cmts] of Object.entries(rules)) {
-    for (const cmt of cmts) {
-      result.push({ time, cmt: String(cmt) });
-    }
-  }
-  return result;
-}
-
-//실제 화면 그리는 Page
-export default async function CmtPage({ params }: { params: Promise<Params> }) {
-  const { time, cmt } = await params;
-
+export default function CommentDetail({
+  params,
+}: {
+  params: Promise<{ time: string; cmt: string }>;
+}) {
+  const { time, cmt } = use(params);
   return (
-    <div>
-      Good {time} - {cmt} comments!
-    </div>
+    <h1>
+      {/* {JSON.stringify(generateStaticParamsX())} */}
+      Comments - ${time} - ${cmt + 10}
+    </h1>
   );
 }
