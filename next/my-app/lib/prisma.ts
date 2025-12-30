96dafcd6448dab7e1032493d9b67c5792cf4a76a
thisminji <thisminji@dgu.ac.kr>
2025-12-30 17:06:09 +0900

    Next: prisma generate

---
+++ next/my-app/lib/prisma.ts
@@ -0,0 +1,11 @@
+// lib/prisma.ts
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
+import { PrismaClient } from './generated/prisma/client';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
