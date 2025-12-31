69ca1650bb1bc42e142558d336d6335a13a69e32
thisminji <thisminji@dgu.ac.kr>
2025-12-31 15:04:03 +0900

    Next : regist

--- next/my-app/app/sign/SignForm.tsx
+++ next/my-app/app/sign/SignForm.tsx
@@ -1,34 +1,30 @@
-'use client';
+"use client";
 
-import type { Route } from 'next';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useActionState } from 'react';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  type EmailPasswd,
-  loginEmail,
-  type ValidError,
-} from '@/lib/sign.action';
+import type { Route } from "next";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useActionState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { loginEmail, type ValidError } from "@/lib/sign.action";
 
 export default function SignForm() {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const redirectTo = searchParams.get('callbackUrl') || '/hello';
+  const redirectTo = searchParams.get("callbackUrl") || "/hello";
 
   const [validError, login, isPending] = useActionState(
-    async (_: ValidError<EmailPasswd> | undefined, formData: FormData) => {
+    async (_: ValidError | undefined, formData: FormData) => {
       const [err, data] = await loginEmail(formData);
-      console.log('🚀 ~ err:', err, data);
+      console.log("🚀 ~ err:", err, data);
       if (err) {
-        return err;
+        return err as ValidError;
       }
 
-      console.log('🚀 ~ redirectTo:', redirectTo);
+      console.log("🚀 ~ redirectTo:", redirectTo);
       router.push(redirectTo as Route);
     },
-    undefined,
+    undefined
   );
 
   return (
@@ -66,11 +62,11 @@ export default function SignForm() {
         </div>
 
         <div className="flex justify-center gap-5">
-          <Button variant={'outline'} type="reset">
+          <Button variant={"outline"} type="reset">
             Cancel
           </Button>
           <Button type="submit" disabled={isPending}>
-            LogIn{isPending && '...'}
+            LogIn{isPending && "..."}
           </Button>
         </div>
       </form>
