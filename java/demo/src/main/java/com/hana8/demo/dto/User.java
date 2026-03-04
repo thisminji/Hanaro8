9b73c504bad1ec5df4663a07ccc514d564ff5302
thisminji <thisminji@dgu.ac.kr>
2026-03-04 10:00:44 +0900

    user

--- java/demo/src/main/java/com/hana8/demo/dto/User.java
+++ java/demo/src/main/java/com/hana8/demo/dto/User.java
@@ -2,6 +2,11 @@ package com.hana8.demo.dto;
 
 import java.time.LocalDate;
 
+import com.hana8.demo.common.serializer.CardnoSerializer;
+import com.hana8.demo.common.serializer.TelnoSerializer;
+import com.hana8.demo.common.validator.Cardno;
+import com.hana8.demo.common.validator.Telno;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,6 +15,7 @@ import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import tools.jackson.databind.annotation.JsonSerialize;
 
 @Data
 @AllArgsConstructor
@@ -30,5 +36,18 @@ public class User {
 	private LocalDate birthdt;
 
 	@NotNull
+	@Telno
+	@JsonSerialize(using = TelnoSerializer.class)
 	private String tel;
+
+	@Cardno
+	@JsonSerialize(using = CardnoSerializer.class)
+	private String creditCard;
+
+	public void replaceSpaceAndHyphen() {
+		this.tel = this.tel.replaceAll("[\\s-]", "");
+
+		if (this.creditCard != null)
+			this.creditCard = this.creditCard.replaceAll("[\\s-]", "");
+	}
 }
