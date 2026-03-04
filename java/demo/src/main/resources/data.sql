efa13410402db3ddc877db3ec3bd940753f51c00
thisminji <thisminji@dgu.ac.kr>
2026-03-04 10:00:37 +0900

    uuid

--- java/demo/src/main/resources/data.sql
+++ java/demo/src/main/resources/data.sql
@@ -1,11 +1,16 @@
 SET FOREIGN_KEY_CHECKS = 0;
 truncate table User;
+truncate table Member;
+truncate table Post;
 SET FOREIGN_KEY_CHECKS = 1;
 
 SET time_zone = 'Asia/Seoul';
 
 insert into User(username, email, telno, bloodType)
-values ('Hong', 'hong@gmail.com', '01012345678', 'A');
+values ('Hong', 'hong@gmail.com', '01012345678', 'A'),
+       ('kim', 'kim@gmail.com', '01012345679', 'B');
 
-insert into User(username, email, telno, bloodType)
-values ('kim', 'kim@gmail.com', '01012345679', 'B');
+insert into Member (nickname, email, passwd, isActive, bloodType)
+values ('hong', 'hong@gmail.com', null, 1, 'A'),
+       ('kim', 'kim@gmail.com', null, 1, 'B'),
+       ('lee', 'lee@gmail.com', null, 1, 'AB');
